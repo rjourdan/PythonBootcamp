52c02b1e1e5e40324038a4433477d22727e7343c
rjourdan <rjourdan@10.33.119.33>
2018-09-04 17:23:45 -0700

    Playing with Strings

---
+++ Python Bootcamp/code/string.py
@@ -0,0 +1,24 @@
+'''
+Created on Sep 4, 2018
+
+@author: rjourdan
+'''
+print('hello')
+
+print("I'm going on a run")
+
+hello = "hello"
+ll = hello[-3:-1]
+print(ll)
+lo = hello[3:5]
+print(lo)
+
+mystring = "Hello World"
+print(len(mystring))
+print(mystring[3:])
+print(mystring[:6])
+#all the way from the beginning, all the way to the end, using step jump of 2
+print(mystring[::2])
+
+#print reverse with step jump of 1
+print(mystring[::-1])
